9dfe0373753719b4978e24f0e89a72199cd5efa7
grafPnd <stupidstyle@yandex.ru>
2016-07-13 14:52:09 +0300

    some new animations

--- data/modules/circle_grow3.js
+++ data/modules/circle_grow3.js
@@ -1,17 +1,22 @@
 $(window).on('runAnimation', function(e, data){
-	if(data.name == 'circle_grow'){
-		// console.log('anim1')
+	if(data.name == 'circle_grow3'){
+		// console.log('anim3')
 		var
 			w = 0, 
-			h = 0,
-			op = 1, 
+			h = 0, 
+			op = 1,
 			ml,mt,br;
 		setDeathTime(data);	
-
+		if(!data.master.fig[2]){
+			data.master.fig[2] = document.createElement('div');
+			data.master.fig[0].appendChild(data.master.fig[2]);
+		}
+		data.master.fig[2].appendStyle({position: 'absolute', display: 'block', width: '100%', height: '100%', background: 'url(img/tile.png) repeat'});
 		data.master.fig[0].appendStyle({background: 'green', overflow: 'hidden', opacity: op, width:  w + 'px', height: h + 'px', top: '50%', left: '50%', marginTop: -mt + 'px', marginLeft: -ml + 'px', position: 'absolute'});
 		data.master.runner = setInterval(function(){
 			if(deathtime(data)){
 				clearInterval(data.master.runner);
+				data.master.fig[2].appendStyle({display: 'none'});
 				$(window).trigger('finishedAnimation');
 			}else{
 				w ++;
--- data/modules/circle_grow4.js
+++ data/modules/circle_grow4.js
@@ -1,16 +1,22 @@
 $(window).on('runAnimation', function(e, data){
-	if(data.name == 'circle_grow2'){
-		// console.log('anim2')
+	if(data.name == 'circle_grow4'){
+		// console.log('anim4')
 		var
 			w = parseInt(data.master.fig[0].style.width, 10), 
 			h = parseInt(data.master.fig[0].style.height, 10), 
 			op = 0,
 			ml,mt,br;
 		setDeathTime(data);	
+		if(!data.master.fig[3]){
+			data.master.fig[3] = document.createElement('div');
+			data.master.fig[1].appendChild(data.master.fig[3]);
+		}
+		data.master.fig[3].appendStyle({position: 'absolute', display:'block', width: '100%', height: '100%', background: 'url(img/tile.png) repeat'});
 		data.master.fig[1].appendStyle({background: 'red', overflow: 'hidden', opacity: op, width:  w + 'px', height: h + 'px', top: '50%', left: '50%', marginTop: -mt + 'px', marginLeft: -ml + 'px', position: 'absolute'});
 		data.master.runner = setInterval(function(){
 			if(deathtime(data)){
 				clearInterval(data.master.runner);
+				data.master.fig[3].appendStyle({display: 'none'});
 				$(window).trigger('finishedAnimation');
 			}else{
 				w --;
